8e4fe182b50d158c7adb858a44e8b7dfae89ba44
Kasper Baun <kasperbaun@hotmail.com>
2022-03-17 10:35:13 +0100

    Trying to customize MudTheme

---
+++ Theme/BmeDarkPalette.cs
@@ -0,0 +1,26 @@
+﻿using MudBlazor;
+namespace BoostMyEconomy.Theme
+
+{
+    public class BmeDarkPalette : Palette
+    {
+        public Palette DarkPalette { get; }
+
+        public BmeDarkPalette()
+        {
+            DarkPalette = new Palette()
+            {  
+                Background = Colors.Grey.Default,
+                Surface = Colors.Grey.Darken1,
+                Primary = Colors.Green.Darken4,
+                Secondary = Colors.Green.Darken1,
+                Tertiary = Colors.Green.Darken1,
+                Info = Colors.Green.Darken1,
+                Success = Colors.Green.Darken1,
+                Warning = Colors.Green.Darken1,
+                Error = Colors.Green.Darken1
+
+            };
+        }
+    }
+}
